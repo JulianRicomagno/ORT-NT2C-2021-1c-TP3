5fd5f8845166842a040e5362f146a69580bd9e9a
juliRicomagno <Julian@JR-GI.tpzargentina.com.ar>
2021-04-10 10:02:43 -0300

    TP3 Julian Ricomagno Terminado

--- script.js
+++ script.js
@@ -5,11 +5,55 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
-
-const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
+const list = document.getElementById('todo-list')
 
+let tareasTotales = 0
+let tareasNoRealizadas = 0
+
+//Ingresamos un mensaje a mostrar
 function addTodo() {
-  alert('Boton Add TODO clickeado!')
+  const task = prompt("Ingrese una tarea");
+  if (task === null) {
+    return;
+  }  
+
+  //Create element div
+  const div = document.createElement("div");
+
+  //Create element li and set className
+  const li = document.createElement("li");
+  li.className = classNames.TODO_ITEM;
+
+  //Create Checkbox and ser properties
+  const check = document.createElement('input');
+  check.type = "checkbox";
+  check.className = classNames.TODO_CHECKBOX;
+  check.onchange = listenerTareas;
+  
+  //Join task/check/li and div
+	li.append(task);
+  li.append(check);
+  div.append(li);
+  list.append(div);
+
+  contadorItems();
+   
+}
+
+function contadorItems(){
+  tareasTotales++;
+  tareasNoRealizadas++;
+  itemCountSpan.innerHTML = tareasTotales;
+  uncheckedCountSpan.innerHTML = tareasNoRealizadas;
+}
+
+function listenerTareas(evento){
+  if(evento.target.checked){
+    tareasNoRealizadas--
+  }else{
+    tareasNoRealizadas++
+  }
+  uncheckedCountSpan.innerHTML = tareasNoRealizadas;
 }
